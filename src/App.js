3fc67fd84668d399c8b69fd83b39d945ab6cb625
Claire Ramming <claire@compstak.com>
2021-09-12 23:35:00 -0400

    allow editing and deleting tasks inline

--- src/App.js
+++ src/App.js
@@ -5,23 +5,60 @@ import TodoItem from './components/TodoItem'
 function App() {
   const origTodos = JSON.parse(localStorage.getItem('todos'))
   const [ todos, setTodos ] = useState(origTodos ? origTodos : [])
-  const [lastTaskId, setLastTaskId] = useState(todos.length > 0 ? todos.at(-1).id : 0)
+  const [ lastTaskId, setLastTaskId ] = useState(todos.length > 0 ? todos.at(-1).id : 0)
   const [ newTask, setNewTask ] = useState()
   const todoList = todos.map(todo => 
     <TodoItem 
       key={todo.id} 
       data={todo} 
       handleChange={handleChange} 
-      handleClick={handleClick}
+      handleClick={removeTask}
+      handleEditTask={editTask}
+      handleUpdate={updateTask}
+      saveTask={saveTask}
     />)
   
-  function handleClick(id) {
+  function removeTask(id) {
     setTodos(prevTodos => {
       const newTodos = prevTodos.filter(todo => todo.id !== id)
       localStorage.setItem('todos', JSON.stringify(newTodos))
       return newTodos
     })
   }
+
+  function editTask(id) {
+    setTodos(prevTodos => {
+      const updateTodos = prevTodos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              edit : true
+            }
+          } else { return {
+            ...todo,
+            edit: false
+          }
+        }
+      })
+      return updateTodos
+    })
+  }
+
+  function updateTask(event, id) {
+    setTodos(prevTodos => {
+      const updateTodos = prevTodos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            text: event.target.value
+          }
+        } else { return todo }
+      })
+      localStorage.setItem('todos', JSON.stringify(updateTodos))
+      return updateTodos
+    })
+  }
+
 
   function updateNewTask(event) {setNewTask(event.target.value)}
 
@@ -56,6 +93,27 @@ function App() {
         }
       )
       setNewTask('')
+    }
+  }
+
+  function saveTask(event, task) {
+    if (event.key === 'Backspace' && !task.text) {
+      removeTask(task.id)
+    }
+    if (event.key === 'Enter') {
+      setTodos(prevTodos => {
+        const updateTodos = prevTodos.map(todo => {
+          if (todo.id === task.id) {
+            return {
+              ...todo,
+              text: event.target.value,
+              edit: false
+            }
+          } else { return todo }
+        })
+        localStorage.setItem('todos', JSON.stringify(updateTodos))
+        return updateTodos
+      })
     }
   }
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,13 +3,25 @@ import Checkbox from './Checkbox'
 
 function TodoItem(props) {
     const itemStyle = props.data.completed ? "completed-item" : "todo-item"
+
     return (
         <div className={itemStyle}>
             <Checkbox 
                 checked={props.data.completed}
                 handleChange = {props.handleChange}
                 id = {props.data.id} />
-            <p>{props.data.text}</p>
+            <p 
+                onClick={()=> props.handleEditTask(props.data.id)} 
+                hidden={props.data.edit}>{props.data.text}</p>
+            <input 
+                type='text'
+                id={props.data.aid}
+                hidden={!props.data.edit} 
+                className='update-task'
+                value={props.data.text}
+                placeholder='what do you want to do?'
+                onChange={(e) => props.handleUpdate(e, props.data.id)}
+                onKeyDown={(e) => props.saveTask(e, props.data)} />
             <button 
                 onClick={() => props.handleClick(props.data.id)}
             >X</button>
