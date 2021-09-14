d390d878055ee2bb5689250fffa33bf5d86df892
Claire Ramming <claire@compstak.com>
2021-09-13 20:09:24 -0400

    allow editing of blank tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -17,19 +17,19 @@ function TodoItem(props) {
                 checked={props.data.completed}
                 handleChange = {props.handleChange}
                 id = {props.data.id} />
-            <p 
-                onClick={()=> props.handleEditTask(props.data.id)} 
-                hidden={props.data.edit}>{props.data.text}</p>
-            <input
-                ref={inputRef}
-                type='text'
-                id={props.data.aid}
-                hidden={!props.data.edit} 
-                className='update-task'
-                value={props.data.text}
-                placeholder='what do you want to do?'
-                onChange={(e) => props.handleUpdate(e, props.data.id)}
-                onKeyDown={(e) => props.saveTask(e, props.data)} />
+                <p 
+                    onClick={()=> props.handleEditTask(props.data.id)} 
+                    hidden={props.data.edit}>{props.data.text}</p>
+                <input
+                    ref={inputRef}
+                    type='text'
+                    id={props.data.aid}
+                    hidden={!props.data.edit} 
+                    className='update-task'
+                    value={props.data.text}
+                    placeholder='what do you want to do?'
+                    onChange={(e) => props.handleUpdate(e, props.data.id)}
+                    onKeyDown={(e) => props.saveTask(e, props.data)} />
             <button 
                 onClick={() => props.handleClick(props.data.id)}
             >X</button>
